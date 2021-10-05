3ab8677d7adf5baa197a74c1352929a1fcc53fc7
JensH <jhj@mindkey.com>
2021-10-05 11:42:07 +0200

    Stuff

---
+++ real/start.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+import os
+# initialize asebamedulla in background and wait 0.3s to let
+# asebamedulla startup
+os.system("(asebamedulla ser:name=Thymio-II &) && sleep 0.3")
+import matplotlib.pyplot as plt
+from time import sleep
+import dbus
+import dbus.mainloop.glib
+from threading import Thread
+
+def sensor_readings_to_motor_speeds(sensor1, sensor2, sensor3, sensor4, sensor5):
+    sensor1 /= 5020
+    sensor2 /= 5020
+    sensor3 /= 5020
+    sensor4 /= 5020
+    sensor5 /= 5020
+    
+    left_mult = 1 - (sensor4 + sensor5) + sensor3 / 10
+    right_mult = 1 - (sensor1 + sensor2) - sensor3 / 10
+    return left_mult, right_mult
+
+class Thymio:
+    def __init__(self):
+        self.aseba = self.setup()
+
+    def drive(self, left_wheel_speed, right_wheel_speed):
+        print("Left_wheel_speed: " + str(left_wheel_speed))
+        print("Right_wheel_speed: " + str(right_wheel_speed))
+
+        left_wheel = left_wheel_speed
+        right_wheel = right_wheel_speed
+
+        self.aseba.SendEventName("motor.target", [left_wheel, right_wheel])
+
+    def stop(self):
+        left_wheel = 0
+        right_wheel = 0
+        self.aseba.SendEventName("motor.target", [left_wheel, right_wheel])
+
+    def get_motor_multipliers(self):
+        prox_horizontal = self.aseba.GetVariable("thymio-II", "prox.horizontal")
+        return sensor_readings_to_motor_speeds(prox_horizontal[0], prox_horizontal[1], prox_horizontal[2], prox_horizontal[3], prox_horizontal[4])
+
+    def sens(self):
+        while True:
+            prox_horizontal = self.aseba.GetVariable("thymio-II", "prox.horizontal")
+            print("Sensing:")
+            print(prox_horizontal[0])
+            print(prox_horizontal[1])
+            print(prox_horizontal[2])
+            print(prox_horizontal[3])
+            print(prox_horizontal[4])
+
+############## Bus and aseba setup ######################################
+
+    def setup(self):
+        print("Setting up")
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        bus = dbus.SessionBus()
+        asebaNetworkObject = bus.get_object("ch.epfl.mobots.Aseba", "/")
+
+        asebaNetwork = dbus.Interface(
+            asebaNetworkObject, dbus_interface="ch.epfl.mobots.AsebaNetwork"
+        )
+        # load the file which is run on the thymio
+        asebaNetwork.LoadScripts(
+            "thympi.aesl", reply_handler=self.dbusError, error_handler=self.dbusError
+        )
+
+        # scanning_thread = Process(target=robot.drive, args=(200,200,))
+        return asebaNetwork
+
+    def stopAsebamedulla(self):
+        os.system("pkill -n asebamedulla")
+
+    def dbusReply(self):
+        # dbus replys can be handled here.
+        # Currently ignoring
+        pass
+
+    def dbusError(self, e):
+        # dbus errors can be handled here.
+        # Currently only the error is logged. Maybe interrupt the mainloop here
+        print("dbus error: %s" % str(e))
+
+
+
+#------------------ Main loop here -------------------------
+
+def main():
+    robot = Thymio()
+
+    #robot.sens()
+
+    thread = Thread(target=robot.sens)
+    thread.daemon = True
+    thread.start()
+    while True:
+        left_multiplier, right_multiplier = robot.get_motor_multipliers()
+        robot.drive(200 * left_multiplier, 200 * right_multiplier)
+        sleep(0.01)
+    robot.stop()
+
+
+#------------------- Main loop end ------------------------
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Stopping robot")
+        exit_now = True
+        sleep(1)
+        os.system("pkill -n asebamedulla")
+        print("asebamodulla killed")
+
+
+## TEST
---
+++ shared/util.py
@@ -0,0 +1,10 @@
+def sensor_readings_to_motor_speeds(sensor1, sensor2, sensor3, sensor4, sensor5):
+    sensor1 /= 5020
+    sensor2 /= 5020
+    sensor3 /= 5020
+    sensor4 /= 5020
+    sensor5 /= 5020
+    
+    left_mult = 1 - (sensor4 + sensor5) + sensor3 / 10
+    right_mult = 1 - (sensor1 + sensor2) - sensor3 / 10
+    return left_mult, right_mult
