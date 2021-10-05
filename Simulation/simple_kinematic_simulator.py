90bf46f92a7d56e63341866fe3b91bca761325a7
JensH <jhj@mindkey.com>
2021-10-05 10:51:50 +0200

    Work

--- Simulation/simple_kinematic_simulator.py
+++ Simulation/simple_kinematic_simulator.py
@@ -19,18 +19,19 @@ simulation_timestep = 0.01  # timestep in kinematics sim (probably don't touch..
 
 # the world is a rectangular arena with width W and height H
 world = LinearRing([(W/2,H/2),(-W/2,H/2),(-W/2,-H/2),(W/2,-H/2)])
+obstacle = LinearRing([(W/4,H/4),(-W/4,H/4),(-W/4,-H/4),(W/4,-H/4)])
 
 import matplotlib.pyplot as plt
 
 # Variables 
 ###########
 
-x = 0.0   # robot position in meters - x direction - positive to the right 
+x = W/3   # robot position in meters - x direction - positive to the right 
 y = 0.0   # robot position in meters - y direction - positive up
-q = 0.0   # robot heading with respect to x-axis in radians 
+q = math.pi   # robot heading with respect to x-axis in radians 
 
-left_wheel_velocity = random()   # robot left wheel velocity in radians/s
-right_wheel_velocity = random()  # robot right wheel velocity in radians/s
+left_wheel_velocity = 1   # robot left wheel velocity in radians/s
+right_wheel_velocity = 1  # robot right wheel velocity in radians/s
 
 # Kinematic model
 #################
@@ -54,28 +55,32 @@ def sensor_readings_to_motor_speeds(sensor1, sensor2, sensor3, sensor4, sensor5)
     sensor3 /= 5020
     sensor4 /= 5020
     sensor5 /= 5020
-    #sensor1, sensor2, sensor3, sensor4, sensor5 = list(map(lambda v: v/5020, (1,2,3,4,5)))
     
-    return 1 - (sensor4 + sensor5), 1 - (sensor1 + sensor2)
+    left_mult = 1 - (sensor4 + sensor5) + sensor3 / 10
+    right_mult = 1 - (sensor1 + sensor2) - sensor3 / 10
+    return left_mult, right_mult
 
 
 def distance_to_sensor_reading(distance):
     max_dist = 0.1
     max_reading = 5020
+    
     if distance > max_dist:
         return 0
     return (1 - (distance / max_dist)) * max_reading
-    
-    
 
 def get_sensor_distance(angle):
     angle = angle / 180 * math.pi # Convert to radians
     #simple single-ray sensor
-    ray = LineString([(x, y), (-(y + sin(q + angle) * 2 * H), x + cos(q + angle) * 2 * W) ])  # a line from robot to a point outside arena in direction of q
-    s = world.intersection(ray)
+    ray = LineString([(x, y), (x + cos(q + angle) * 2 * W, (y + sin(q + angle) * 2 * H)) ])  # a line from robot to a point outside arena in direction of q
+    s = world.union(obstacle).intersection(ray)
+    
+    if type(s) is shapely.geometry.multipoint.MultiPoint:
+        s = s[-1]
+
     distance = sqrt((s.x-x)**2+(s.y-y)**2) - 0.05                    # distance to wall
 
-    return distance# distance_to_sensor_reading(distance)
+    return distance_to_sensor_reading(distance)
 
 # Simulation loop
 #################
@@ -83,36 +88,34 @@ file = open("trajectory.dat", "w")
 
 for cnt in range(5000):
     #simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
-    # sensor1 = get_sensor_distance(30)
-    # sensor2 = get_sensor_distance(15)
+    sensor1 = get_sensor_distance(30)
+    sensor2 = get_sensor_distance(15)
     sensor3 = get_sensor_distance(0)
-    # sensor4 = get_sensor_distance(-15)
-    # sensor5 = get_sensor_distance(-30)
+    sensor4 = get_sensor_distance(-15)
+    sensor5 = get_sensor_distance(-30)
     
 
-    #print(sensor1, sensor2, sensor3, sensor4, sensor5)
-    left_mult, right_mult = (1, 1) # sensor_readings_to_motor_speeds(sensor1, sensor2, sensor3, sensor4, sensor5)
-    print(sensor3)
-    # print(left_mult, right_mult)
-    if cnt%1000==0:
-        left_wheel_velocity = random()
-        right_wheel_velocity = random()
-        
+    ## print(sensor1, sensor2, sensor3, sensor4, sensor5)
+    left_mult, right_mult = sensor_readings_to_motor_speeds(sensor1, sensor2, sensor3, sensor4, sensor5)
+    print(left_mult, right_mult, sensor3, q * 180/math.pi)
+    # if cnt%1000==0:
+    #     left_wheel_velocity = random()
+    #     right_wheel_velocity = random()
+    # left_wheel_velocity *= left_mult
+    # right_wheel_velocity *= right_mult
     #step simulation
     simulationstep(left_wheel_velocity * left_mult, right_wheel_velocity * right_mult)
 
     #check collision with arena walls
     if (world.distance(Point(x,y))<L/2):
+        file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.05) + ", " + str(sin(q)*0.05) + "\n")
         print('Collision!', q, x, y)
-        ray = LineString([(x, y), (x+cos(q)*2*W,-(y+sin(q)*2*H)) ])  # a line from robot to a point outside arena in direction of q
-        plt.plot(*world.xy)
-        plt.plot(*ray.xy)
-        plt.show()
+        print(cnt, 'Steps')
         break
         
-    if cnt%50==0:
+    if cnt % 50 == 0:
         file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.05) + ", " + str(sin(q)*0.05) + "\n")
-
+    
 file.close()
     
 import visualizer
