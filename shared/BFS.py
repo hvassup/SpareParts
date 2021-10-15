de63f6cf049af16190648814b51346e48d190579
JensH <jhj@mindkey.com>
2021-10-15 13:08:41 +0200

    +BFS +Map

---
+++ shared/BFS.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+# BFS which finds the first path that solves the game
+def BFS(start_pos, to_pos, get_next_states):
+    path = (start_pos, )
+    queue = deque([])
+    queue.append((start_pos, path))
+
+    explored = set()
+
+    while len(queue) > 0:
+        current_robot_pos, path = queue.popleft()
+
+        if current_robot_pos == to_pos:
+            print('Done! :)')
+            return path
+
+        x, y = current_robot_pos
+        next_states = get_next_states(x, y)
+
+        for next_robot_pos in next_states:
+            if next_robot_pos in explored:
+                continue
+
+            explored.add(next_robot_pos)
+
+            new_path = path + (next_robot_pos,)
+            queue.append((next_robot_pos, new_path))
+
+    print('Something went wrong! :(', path)
+    return 'Oops'
---
+++ shared/map.py
@@ -0,0 +1,37 @@
+import numpy as np
+from BFS import BFS
+
+# Width and Height of map in CM
+MAP_WIDTH = 100
+MAP_HEIGHT = 200
+
+# Resolution: How many cm per grid
+MAP_RESOLUTION = 1
+
+# Create map of zeros
+map = np.zeros((MAP_WIDTH // MAP_RESOLUTION, MAP_HEIGHT // MAP_RESOLUTION))
+
+# Mark coordinate on map as dangerous (mark as a 1)
+def mark_as_danger(x, y):
+    map[y][x] = 1
+    
+# Mark coordinate on map as safe (mark as a 0)
+def mark_as_safe(x, y):
+    map[y][x] = 0
+
+# Return whether coordinate on map is a danger area
+def is_danger(x, y):
+    if x < 0 or y < 0 or y >= map.shape[0] or x >= map.shape[1]:
+        return True
+    return map[y][x] == 1
+
+# Get all neighbor fields that are not marked as dangerous
+def get_safe_neighbors(x, y):
+    neighbor_coords = [(x + 1, y), (x - 1, y), (x, y - 1), (x, y + 1)]
+    neighbors = filter(lambda i: not is_danger(i[0], i[1]), neighbor_coords)
+    return neighbors
+
+# Run BFS from a starting location to a target location for example: find_safe_path((0, 0), (50, 50))
+# Returns a tuple containing the safe path
+def find_safe_path(fromPoint, toPoint):
+    return BFS(fromPoint, toPoint, get_safe_neighbors)
