99c8e932ce56e065a46976ec1729f0fb1795b305
Jens <jhj@mindkey.com>
2021-10-30 16:04:03 +0200

    rename Simulation to simulation

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 
+
 def run(is_simulation):
     if is_simulation:
         import simulation.simple_kinematic_simulator
     else:
         import real.start
+
 
 if len(sys.argv) == 2 and sys.argv[1] == 'Simulation':
     run(is_simulation=True)
